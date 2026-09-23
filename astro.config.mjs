aa9489c75ed50e6acc71c77d4eb92505e5e2b7b5
BogMarDev <166923629+BogMarDev@users.noreply.github.com>
2026-09-23 23:36:59 +0200

    Set site and base URL in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,8 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://bogmardev.github.io',
+  base: '/portfolio',
   integrations: [tailwind(), react()],
   vite: {
     resolve: {
